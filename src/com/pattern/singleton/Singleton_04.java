92e619a393815f03d5e8846cf948d22e06f74a9b
zito-s <jhhj424@naver.com>
2021-10-28 22:50:27 +0900

    demo(Singleton): double checked locking 사용
    synchronized 의 범위를 좁혀서 사용하므로써 3번 예제보다 효율적임

---
+++ src/com/pattern/singleton/Singleton_04.java
@@ -0,0 +1,20 @@
+package com.pattern.singleton;
+
+public class Singleton_04 {
+
+    private static Singleton_04 instance;
+
+    private Singleton_04() {
+    }
+
+    public static Singleton_04 getInstance() {
+        if (instance == null) {
+            synchronized (Singleton_04.class) {
+                if (instance == null) {
+                    return new Singleton_04();
+                }
+            }
+        }
+        return instance;
+    }
+}
